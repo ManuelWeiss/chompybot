c8e0ef8682796c372cc9c43c776c86e31723b86d
Manuel Weiss <mweiss@moonfruit.com>
2014-10-20 09:32:28 +0100

    move to Heroku

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,23 +1,28 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{ Action, Controller }
 import play.api.Logger
 import play.api.libs.json.Json
+import play.api.libs.ws.WS
+
 
 object Application extends Controller {
-  def index = Action {
-    Ok(views.html.index("Hello Play Framework"))
-  }
-  
-  def capabilities = Action {
-  	Ok("""
-  	{
+
+    val URL = "https://dry-sierra-3468.herokuapp.com/"
+
+    def index = Action {
+        Ok(views.html.index("Hello Play Framework"))
+    }
+
+    def capabilities = Action {
+        Ok("""
+    {
   "name": "Chompybot",
   "description": "An add-on that makes suggestions for lunch",
   "key": "io.weiss.chompybot",
   "links": {
-    "homepage": "https://example.com/myaddon",
-    "self": "https://11f981ac.ngrok.com/capabilities"
+    "homepage": "https://dry-sierra-3468.herokuapp.com/",
+    "self": "https://dry-sierra-3468.herokuapp.com/capabilities"
   },
   "capabilities": {
     "hipchatApiConsumer": {
@@ -26,41 +31,46 @@ object Application extends Controller {
       ]
     },
     "installable": {
-      "callbackUrl": "https://11f981ac.ngrok.com/installable"
-	},
-	"webhook": [{
-		"url": "https://11f981ac.ngrok.com/message",
-		"event": "room_message",
-		"name": "messages"
-	}]
+      "callbackUrl": "https://dry-sierra-3468.herokuapp.com/installable"
+    },
+    "webhook": [{
+        "url": "https://dry-sierra-3468.herokuapp.com/message",
+        "event": "room_message",
+        "name": "messages"
+    }]
   }
-	}
-	""")
-  }
-  
-  // POST
-  def installable = Action(parse.json) {
-    request => {
-      val json = request.body
-      // log the received credentials:
-      Logger.debug(Json.prettyPrint(json))
-      Ok("success")
     }
-  }
-
-  // POST
-  def message = Action(parse.json) {
-    request => {
-      val json = request.body
-      Logger.debug(Json.prettyPrint(json))
-      Ok("success")
+    """)
     }
-  }
 
-//  def get = Action {
-//    Ok(PersistentCounter.get.toString).withHeaders("X-HELLO-MOBIFY-ROBOT" -> "hi")
-//  }
+    // receives POST
+    def installable = Action(parse.json) {
+        request =>
+            {
+                val json = request.body
+                // log the received credentials:
+                Logger.debug(Json.prettyPrint(json))
+                Ok("success")
+            }
+    }
 
+    // receives POST
+    def receive_message = Action(parse.json) {
+        request =>
+            {
+                val json = request.body
+                Logger.debug(Json.prettyPrint(json))
+                Ok("success")
+            }
+    }
 
+//    def send_message(message: String) = Action {
+//        Async {
+//            val messageURL = "http://"
+//            WS.url(messageURL).get().map { response =>
+//                Ok("Feed title: " + (response.json \ "title").as[String])
+//            }
+//        }
+//    }
 
 }
